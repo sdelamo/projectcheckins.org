a7b5a2ff1d648451dc7dc421862fd8912ea9ec1f
Sergio del Amo <sergio.delamo@softamo.com>
2024-04-29 09:58:09 +0200

    extract methods in ProfileController (#103)
    * extract methods in ProfileController
    
    * Update http/src/main/java/org/projectcheckins/http/controllers/ProfileController.java
    
    Co-authored-by: Guillermo Calvo <guillermo@guillermo.dev>
    
    * Update http/src/main/java/org/projectcheckins/http/controllers/ProfileController.java
    
    Co-authored-by: Guillermo Calvo <guillermo@guillermo.dev>
    
    * Update http/src/main/java/org/projectcheckins/http/controllers/ProfileController.java
    
    Co-authored-by: Guillermo Calvo <guillermo@guillermo.dev>
    
    ---------
    
    Co-authored-by: Guillermo Calvo <guillermo@guillermo.dev>

--- http/src/main/java/org/projectcheckins/http/controllers/ProfileController.java
+++ http/src/main/java/org/projectcheckins/http/controllers/ProfileController.java
@@ -2,9 +2,9 @@ package org.projectcheckins.http.controllers;
 
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.*;
 import io.micronaut.multitenancy.Tenant;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
@@ -17,34 +17,37 @@ import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 import org.projectcheckins.annotations.GetHtml;
 import org.projectcheckins.annotations.PostForm;
 import org.projectcheckins.bootstrap.Breadcrumb;
 import org.projectcheckins.core.api.Profile;
 import org.projectcheckins.core.forms.ProfileUpdate;
 import org.projectcheckins.core.repositories.ProfileRepository;
+import static org.projectcheckins.http.controllers.ApiConstants.*;
 
 @Controller
 class ProfileController {
 
     private static final String PROFILE = "profile";
-    private static final String PATH = ApiConstants.SLASH + PROFILE;
+    private static final String PATH = SLASH + PROFILE;
 
     private static final String MODEL_PROFILE = "profile";
 
     // SHOW
     private static final Message MESSAGE_SHOW = Message.of("Profile", PROFILE + ApiConstants.DOT + ApiConstants.ACTION_SHOW);
-    private static final String PATH_SHOW = PATH + ApiConstants.SLASH + ApiConstants.ACTION_SHOW;
+    private static final String PATH_SHOW = PATH + SLASH + ApiConstants.ACTION_SHOW;
     private static final String VIEW_SHOW = PATH + ApiConstants.VIEW_SHOW;
 
     // EDIT
     private static final Message MESSAGE_BREADCRUMB_EDIT = Message.of("Edit", PROFILE + ApiConstants.DOT + ApiConstants.ACTION_EDIT);
     private static final Breadcrumb BREADCRUMB_EDIT = new Breadcrumb(MESSAGE_BREADCRUMB_EDIT);
-    private static final String PATH_EDIT = PATH + ApiConstants.SLASH + ApiConstants.ACTION_EDIT;
+    private static final String PATH_EDIT = PATH + SLASH + ApiConstants.ACTION_EDIT;
     private static final String VIEW_EDIT = PATH + ApiConstants.VIEW_EDIT;
 
     // UPDATE
-    private static final String PATH_UPDATE = PATH + ApiConstants.SLASH + ApiConstants.ACTION_UPDATE;
+    private static final String PATH_UPDATE = PATH + SLASH + ApiConstants.ACTION_UPDATE;
 
     private final FormGenerator formGenerator;
     private final ProfileRepository profileRepository;
@@ -54,27 +57,33 @@ class ProfileController {
         this.profileRepository = profileRepository;
     }
 
-    @GetHtml(uri = PATH_SHOW, rolesAllowed = SecurityRule.IS_AUTHENTICATED, view = VIEW_SHOW)
-    HttpResponse<?> profileShow(@NonNull @NotNull Authentication authentication, @Nullable Tenant tenant) {
-        return profileRepository.findByAuthentication(authentication, tenant)
-            .map(p -> HttpResponse.ok(Map.of(
-                    MODEL_PROFILE, p,
-                    ApiConstants.MODEL_BREADCRUMBS, List.of(HomeController.BREADCRUMB_HOME, new Breadcrumb(MESSAGE_SHOW)))
-            ))
-            .orElseGet(NotFoundController::notFoundRedirect);
+    @GetHtml(uri = PATH_SHOW, rolesAllowed = SecurityRule.IS_AUTHENTICATED)
+    HttpResponse<?> profileShow(@NonNull @NotNull HttpRequest<?> request,
+                                @NonNull @NotNull Authentication authentication,
+                                @Nullable Tenant tenant) {
+        return showModel(authentication, tenant)
+                .map(model -> new ModelAndView<>(VIEW_SHOW, model))
+                .map(HttpResponse::ok)
+                .orElseGet(NotFoundController::notFoundRedirect);
     }
 
-    @GetHtml(uri = PATH_EDIT, rolesAllowed = SecurityRule.IS_AUTHENTICATED, view = VIEW_EDIT)
-    HttpResponse<?> profileEdit(@NonNull @NotNull Authentication authentication, @Nullable Tenant tenant) {
+    @GetHtml(uri = PATH_EDIT, rolesAllowed = SecurityRule.IS_AUTHENTICATED)
+    HttpResponse<?> profileEdit(@NonNull @NotNull HttpRequest<?> request,
+                                @NonNull @NotNull Authentication authentication,
+                                @Nullable Tenant tenant) {
         return profileRepository.findByAuthentication(authentication, tenant)
-            .map(p -> HttpResponse.ok(new ModelAndView<>(VIEW_EDIT, updateModel(p))))
-            .orElseGet(NotFoundController::notFoundRedirect);
+                .map(this::updateModel)
+                .map(model -> new ModelAndView<>(VIEW_EDIT, model))
+                .map(HttpResponse::ok)
+                .orElseGet(NotFoundController::notFoundRedirect);
     }
 
     @PostForm(uri = PATH_UPDATE, rolesAllowed = SecurityRule.IS_AUTHENTICATED)
-    HttpResponse<?> profileUpdate(@NonNull @NotNull Authentication authentication,
-                                  @NonNull @NotNull @Valid @Body ProfileUpdate profileUpdate,
-                                  @Nullable Tenant tenant) {
+    HttpResponse<?> profileUpdate(
+            @NonNull @NotNull HttpRequest<?> request,
+            @NonNull @NotNull Authentication authentication,
+            @NonNull @NotNull @Valid @Body ProfileUpdate profileUpdate,
+            @Nullable Tenant tenant) {
         profileRepository.update(authentication, profileUpdate, tenant);
         return HttpResponse.seeOther(URI.create(PATH_SHOW));
     }
@@ -93,5 +102,14 @@ class ProfileController {
                 ApiConstants.MODEL_FORM, form,
                 ApiConstants.MODEL_BREADCRUMBS, List.of(HomeController.BREADCRUMB_HOME, new Breadcrumb(MESSAGE_SHOW, PATH_SHOW), BREADCRUMB_EDIT)
         );
+    }
+
+    private Optional<Map<String, Object>> showModel(@NonNull Authentication authentication,
+                                                    @Nullable Tenant tenant) {
+        return profileRepository.findByAuthentication(authentication, tenant)
+                .map(p -> Map.of(
+                        MODEL_PROFILE, p,
+                        ApiConstants.MODEL_BREADCRUMBS, List.of(HomeController.BREADCRUMB_HOME, new Breadcrumb(MESSAGE_SHOW)))
+                );
     }
 }
